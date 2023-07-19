5a93ec95353d1a6c52b43dc3204c8e599f3f0c75
dbsdud00 <dbsdudsw@icloud.com>
2023-07-19 09:16:58 +0900

    myCar

---
+++ SpMVC_004_MyCarV2/src/main/java/com/callor/car/persistance/CarDao.java
@@ -0,0 +1,12 @@
+package com.callor.car.persistance;
+
+import com.callor.car.model.CarDto;
+
+public interface CarDao {
+	
+	public int insert(CarDto carDto);
+	
+	public CarDto findTachoByCarNum(String carnum);
+	
+	
+}
